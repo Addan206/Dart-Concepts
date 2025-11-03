819bd9677fd8bb77ed9e4e7abb043afa5c6eac4d
Addan206 <105232087+Addan206@users.noreply.github.com>
2025-11-03 14:37:44 +0500

    first commit

---
+++ dt.dart
@@ -0,0 +1,29 @@
+void main () {
+  // String
+  String firstname = "Addan";
+  print("String: $firstname");
+
+  // Integer
+  int myNum = 11;
+  print("Integer : $myNum");
+  // Double
+
+  double otherNum = 23.00;
+  print("Double: $otherNum");
+  // Boolean
+  bool myBool = false;
+  print("boolean: $myBool");
+
+  //dynamic
+  dynamic fullName = "M Addan";
+  print("dynamic: $fullName");
+
+
+
+
+
+
+
+
+
+}
